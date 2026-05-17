6ee9bb02e6a35ae5a6693f0fdfac221ca9b471e9
King-Mandeep <mandeeptiwari2006@gmail.com>
2026-05-17 15:00:17 +0530

    updating the import name of file

--- sprint6/shopzone/src/pages/Shop.jsx
+++ sprint6/shopzone/src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/ProductCard.jsx";
 
 function Shop() {
   const [products, setProducts] = useState([]);
